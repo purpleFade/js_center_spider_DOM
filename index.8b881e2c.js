bd7ab475b3b7563bd42af4a3446819f70d5a206e
purplefade <tarovykanton@gmail.com>
2024-01-15 15:37:42 +0200

    make build

---
+++ index.8b881e2c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.getElementById("spider"),i=e.getBoundingClientRect(),l=t.getBoundingClientRect(),n=(i.height-l.height)/2,d=(i.width-l.width)/2;t.style.top=n+"px",t.style.left=d+"px";
+//# sourceMappingURL=index.8b881e2c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+// write code here
+const wall = document.querySelector(".wall");
+const spider = document.getElementById("spider");
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+const topPosition = (wallRect.height - spiderRect.height) / 2;
+const leftPosition = (wallRect.width - spiderRect.width) / 2;
+spider.style.top = topPosition + "px";
+spider.style.left = leftPosition + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
